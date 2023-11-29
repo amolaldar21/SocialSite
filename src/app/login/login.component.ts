d00bdfb198b7274710108cad6551e3e512abede6
amolaldar21 <amol21.seo@gmail.com>
2023-11-30 01:26:04 +0530

    login functionality added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { SingUp } from '../model/signInUp';
 import{ NgForm} from '@angular/forms'
 import{ServiceService} from '../services/service.service'
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -11,18 +12,72 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  islogin:boolean=true;
+  displayPawwordFeild:boolean=false;
+  isdisabled:boolean=true
+  passwordType:string="text"
+  eyeshow:boolean=true
   
+loginData={
+  "username":"",
+  "password":""
 
-  constructor(private service:ServiceService,private router:Router) { }
+}
+
+  constructor(private service:ServiceService,private router:Router ,private _http:HttpClient) { }
 
   ngOnInit() {
   }
+loginInfo(data:any){
+  this.loginData=data
+  console.log(data);
+  console.log(this.loginData);
+  
+  
+ this._http.get<any[]>(this.service.url).subscribe(responce=>{
+  const valid=responce.find((a:any)=>{
+    // console.log(a.email,a.password);
+    // console.log(this.loginData.username,this.loginData.username);
+    
+    
+    return a.email==this.loginData.username && a.password==this.loginData.password
+  })
+  if(valid){
+    alert("Login Successfully")
+    this.router.navigate([""])
+  }
+  else{
+    alert("invalid details")
+    
+    
+  }
+ })
+  
+  
+  
 
+ 
+ 
+ 
+}
+  passwordToggle(){
+    if(this.passwordType=="text"){
+      this.passwordType="password";
+      this.eyeshow=false;
 
-SignUpData(data:SingUp,form: NgForm){
+    }
+    else{
+      this.passwordType="text";
+      this.eyeshow=true;
+    }
+  }
+SignUpData(data:SingUp,form:NgForm){
+  console.log(data);
    this.service.Sentdata(data,form)
+  //  alert("SingUp Successfully")
+   this.router.navigate(['login'])
   //  this.signUpForm.resetForm();
-  // console.log(data);
+   
   
   
 
@@ -36,7 +91,18 @@ navigation()
   // window.location.href='http://localhost:4200';
 }
 
+signOrSingUp(){
+  this.islogin=!this.islogin
+}
+nextField(){
 
+  this.displayPawwordFeild=! this.displayPawwordFeild
+  this.isdisabled=!this.isdisabled
+}
+// returntoInput()
+// {
+//   this.displayPawwordFeild=false
+// }
 
 }
 
--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import{HttpClient}  from '@angular/common/http'
 import { SingUp } from '../model/signInUp';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +16,13 @@ export class ServiceService {
 
   Sentdata(data: SingUp,form: NgForm){
     this._http.post<SingUp>(this.url,data).subscribe((response)=>{
-form.reset()
+
 
 
     })
+    form.reset()
     
     
   }
+  
 }
